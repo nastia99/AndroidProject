75666264300c2044e3fa1e5cdee9a955a6275bfb
Mana Nastassia <nastassia.mana@etu.unice.fr>
2019-03-30 11:00:41 +0100

    creation webview + gradient bg + parameters section and page

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addEditListener(){
+
+        Button parameters = findViewById(R.id.paramButton);
+        Button play = findViewById(R.id.playButton);
+        Button rules = findViewById(R.id.rulesButton);
+
+        parameters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), ParametersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        rules.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), MyWebViewActivity.class);
+                intent.putExtra("url", "https://git-iutinfo.unice.fr/mn707438/androidproject/blob/master/Règles.pdf");
+                startActivity(intent);
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), QCMActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/androidproject/MyWebViewActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyWebViewActivity extends AppCompatActivity {
+
+    private WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview_layout);
+
+        String url= getIntent().getStringExtra("url");
+        webview= (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        WebSettings websettings= webview.getSettings();
+        websettings.setJavaScriptEnabled(true);
+        websettings.setPluginState(WebSettings.PluginState.ON);
+        webview.loadUrl("http://docs.google.com/gview?embedded=true&url="+url);
+
+    }
+}
